f65864dd670869b1cbf30fe39b07174a520f6348
Mahicode0712 <cloud.mahisingh@gmail.com>
2026-03-23 23:56:43 +0530

    third commit

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App /> // This is the main component that will be rendered in the root element
   </StrictMode>,
 )
