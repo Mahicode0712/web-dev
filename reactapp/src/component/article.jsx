25f9577d7d6341d1ee171d144bae58dd029c2d07
Mahicode0712 <cloud.mahisingh@gmail.com>
2026-04-15 22:49:22 +0530

    triumph

--- reactapp/src/component/article.jsx
+++ reactapp/src/component/article.jsx
@@ -1,70 +1,18 @@
 import React, { useState } from "react";
 import style from "../CSS/Article.module.css";
 import style1 from "../CSS/Homepage.module.css";
-import styled from "styled-components";
+//import styled from "styled-components"
+import styled from "@emotion/styled"
+import { css } from '@emotion/react'
 
-// function Article(props) {
-//   const [count, setCount]=useState(0)
 
-//   const handleIncrement=()=>{
-//     setCount(count+1)
-//   }
-//   return (
-//     <div>
-//       <h1>Article</h1>
-//        <h3>{props.title}</h3>
-//       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi eligendi fugiat pariatur eos quibusdam ratione possimus adipisci, impedit, autem ad quo, et cum laudantium deleniti. Soluta molestias amet quas labore.</p>
-//       <h2>{count}</h2>
-//       <button className={style.btn} onClick={handleIncrement}>Increment</button>
-//        <button className={style1.btn}>Decrement</button>
-
-//     </div>
-//   )
-// }
-// export default Article
-
-// import React from 'react'
-// import { add as addition } from '../utility'
-
-class Article extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-      name: "Alex",
-    };
-    //   console.log(addition(2,3))
+function Article(props) {
+  const [count, setCount]=useState(0)
+  
+  const handleIncrement=()=>{
+    setCount(count+1)
   }
-
-  componentDidMount() {
-    console.log("componentDidMount");
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    if (nextState.count === 1) return false;
-    console.log("shouldComponentUpdate");
-    return true;
-  }
-
-  componentDidUpdate() {
-    console.log("componentDidUpdate");
-  }
-
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-  }
-
-  handleClick = () => {
-    // this.state.count=this.state.count+1
-    // console.log(this.state.count)
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  handleDecrement = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
-  render() {
-    return (
+   return (
       <div>
         <h1>Article</h1>
         <h3>{this.state.count}</h3>
@@ -79,13 +27,64 @@ class Article extends React.Component {
       </div>
     );
   }
-}
+
 
 export default Article;
 
+
+const Button1=styled.button`
+background-color: ${props=>props.yellow ? "yellow" : "red"};  
+color: white;
+width: 100px;
+height: 30px;
+`
+
 const Button=styled.button`
-background-color:blue;
-color:white;
-padding:10px;
-border-radius:5px;
-`;
+background-color: ${props=>props.styled ? "green" : "blue"};  
+color: white;
+width: 100px;
+height: 30px;
+`
+
+
+// import React from 'react'
+// import { add as addition } from '../utility'
+
+// class Article extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0,
+//       name: "Alex",
+//     };
+//     //   console.log(addition(2,3))
+//   }
+
+//   componentDidMount() {
+//     console.log("componentDidMount");
+//   }
+
+//   shouldComponentUpdate(nextProps, nextState) {
+//     if (nextState.count === 1) return false;
+//     console.log("shouldComponentUpdate");
+//     return true;
+//   }
+
+//   componentDidUpdate() {
+//     console.log("componentDidUpdate");
+//   }
+
+//   componentWillUnmount() {
+//     console.log("componentWillUnmount");
+//   }
+
+//   handleClick = () => {
+//     // this.state.count=this.state.count+1
+//     // console.log(this.state.count)
+//     this.setState({ count: this.state.count + 1 });
+//   };
+
+//   handleDecrement = () => {
+//     this.setState({ count: this.state.count - 1 });
+//   };
+ 
